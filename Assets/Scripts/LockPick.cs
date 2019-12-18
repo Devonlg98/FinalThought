de14f18027caac7555b2876a0f4da63b68121be8
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 06:36:01 -0800

    is it the first commit tho?

---
+++ Assets/Scripts/LockPick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockPick : MonoBehaviour
+{
+    public GameObject lockPicker;
+    public GameObject lockPad;
+    private Minigame mini;
+    private int pickCount;
+    bool stickUpDown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mini = GetComponent<Minigame>();
+        stickUpDown = false;
+        pickCount = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis(mini.yAxis) >= 0.25f && stickUpDown == false)
+        {
+            Debug.Log("Stick Up");
+            pickCount++;
+            stickUpDown = true;
+        }
+
+        if (Input.GetAxis(mini.yAxis) <= -0.25f && stickUpDown == true)
+        {
+            Debug.Log("Stick Down");
+            pickCount++;
+            stickUpDown = false;
+        }
+    }
+    private void Reset()
+    {
+        stickUpDown = false;
+        pickCount = 0;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Player[] players;
+    public GameObject[] minigames;
+    public static GameObject encIntro;
+    public static AudioClip minigameWinSnd;
+    public static AudioClip minigameLoseSnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(players[0].pickedUpPowerUp == true)
+        {
+            Debug.Log("Player1 slow manager??");
+            AssignMinigame(1);
+            AssignMinigame(2);
+            AssignMinigame(3);
+            players[0].pickedUpPowerUp = false;
+        }
+        if (players[0].encounterTrigger == true)
+        {
+            if (!players[0].inMinigame)
+            {
+                AssignMinigame(0);
+                players[0].encounterTrigger = false;
+            }
+        }
+
+
+        if (players[1].pickedUpPowerUp == true)
+        {
+            AssignMinigame(0);
+            AssignMinigame(2);
+            AssignMinigame(3);
+            players[1].pickedUpPowerUp = false;
+        }
+        if (players[1].encounterTrigger == true)
+        {
+            if (!players[1].inMinigame)
+            {
+                AssignMinigame(1);
+                players[1].encounterTrigger = false;
+            }
+        }
+
+        if (players[2].pickedUpPowerUp == true)
+        {
+            AssignMinigame(1);
+            AssignMinigame(0);
+            AssignMinigame(3);
+            players[2].pickedUpPowerUp = false;
+        }
+        if (players[2].encounterTrigger == true)
+        {
+            if (!players[2].inMinigame)
+            {
+                AssignMinigame(2);
+                players[2].encounterTrigger = false;
+            }
+        }
+
+        if (players[3].pickedUpPowerUp == true)
+        {
+            AssignMinigame(1);
+            AssignMinigame(2);
+            AssignMinigame(0);
+            players[3].pickedUpPowerUp = false;
+        }
+        if (players[3].encounterTrigger == true)
+        {
+            if (!players[3].inMinigame)
+            {
+                AssignMinigame(3);
+                players[3].encounterTrigger = false;
+            }
+        }
+    }
+    void AssignMinigame(int player)
+    {
+        int butt;
+        butt = (int)Random.Range(0.0f, (float)minigames.Length - 1.0f);
+        while (minigames[butt].activeSelf)
+        {
+            butt = (int)Random.Range(0.0f, (float)minigames.Length - 1.0f);
+            Debug.Log(butt);
+        }
+        //minigames[butt].SetActive(true);
+        minigames[butt].GetComponent<Minigame>().TimerSet();
+        minigames[butt].GetComponent<Minigame>().finish = false;
+        players[player].SetMini(minigames[butt].GetComponent<Minigame>());
+        Vector3 pos = new Vector3(players[player].miniWindow.transform.position.x, players[player].miniWindow.transform.position.y, 1);
+        minigames[butt].transform.position = pos;
+    }
+}
